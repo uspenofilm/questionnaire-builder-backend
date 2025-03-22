dd0c26e55496c5191c2ad422d8f38516461788e0
uspenofilm <mariia.terebus@gmail.com>
2025-03-22 20:00:23 +0200

    connected to mongo

---
+++ src/db/models/quiz.js
@@ -0,0 +1,51 @@
+import { model, Schema } from 'mongoose';
+
+const quizSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    questionsCount: {
+      type: Number,
+      required: true,
+    },
+    completionsCount: {
+      type: Number,
+      required: false,
+    },
+    questions: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+        label: {
+          type: String,
+          required: true,
+        },
+        type: {
+          type: String,
+          required: true,
+          enum: ['text', 'single', 'multiple'],
+        },
+        options: [
+          {
+            type: String,
+            required: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const QuizCollection = model('quiz', quizSchema);
